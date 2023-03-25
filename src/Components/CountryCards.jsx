35554ff827216602d4e36707482ede13d2bbf28e
Amadi Jerry <amadijerry310@gmail.com>
2023-03-25 12:32:30 +0100

    completed card details component

--- src/Components/CountryCards.jsx
+++ src/Components/CountryCards.jsx
@@ -31,7 +31,7 @@ const CountryCards = ({region}) => {
   return (
     <div className='pt-8 mx-auto w-[90%] grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-4'>
         {!countryData && <div className="h-screen flex items-center justify-center dark:bg-very-dark-blue mt-4">
-            <button type="button" class="dark:bg-dark-blue bg-white shadow-lg animate-pulse rounded-md p-4 text-very-dark-blue-light dark:text-white" disabled>
+            <button type="button" class="dark:bg-dark-blue mx-auto bg-white shadow-lg animate-pulse rounded-md p-4 text-very-dark-blue-light dark:text-white" disabled>
            Loading...
            </button>
         </div>
@@ -41,9 +41,9 @@ const CountryCards = ({region}) => {
                 <img src={country.flags.svg} alt="" className="rounded-t-md h-[170px] w-[100%] m-0" />
                 <div className="mt-6 px-6 pb-12 text-very-dark-blue-light dark:text-white">
                     <h2 className="text-lg font-bold">{country.name.common}</h2>
-                    <p className="opacity-[0.8] mt-4">Population: <span className="opacity-[0.6]">{country.population.toLocaleString()}</span></p>
-                    <p className="opacity-[0.8]">Region: <span className="opacity-[0.6]">{country.region}</span></p>
-                    <p className="opacity-[0.8]">Capital: <span className="opacity-[0.6]">{country.capital}</span></p>
+                    <p className="opacity-[0.8] mt-4">Population: <span className="opacity-[0.4]">{country.population.toLocaleString()}</span></p>
+                    <p className="opacity-[0.8]">Region: <span className="opacity-[0.4]">{country.region}</span></p>
+                    <p className="opacity-[0.8]">Capital: <span className="opacity-[0.4]">{country.capital}</span></p>
                 </div>
             </div>
         ))}
--- src/Components/CountryDetails.jsx
+++ src/Components/CountryDetails.jsx
@@ -30,27 +30,45 @@ const CountryDetails = () => {
     },[id])
 
   return (
-    <div className="h-screen p-6 bg-very-light-gray dark:bg-very-dark-blue">
-        {!countryData && <div className="h-screen flex items-center justify-center dark:bg-very-dark-blue mt-4">
+    <div className="h-[100%] p-8 lg:px-16 bg-very-light-gray dark:bg-very-dark-blue">
+        {!countryData && <div className="h-screen flex items-center justify-center dark:bg-very-dark-blue mt-4 w-[120px] mx-auto">
             <button type="button" class="dark:bg-dark-blue bg-white shadow-lg animate-pulse rounded-md p-4 text-very-dark-blue-light dark:text-white" disabled>
            Loading...
            </button>
         </div>}
         {countryData && <div >
-            <button className="text-very-dark-blue-light dark:text-white p-4 rounded-sm text-center mt-4 bg-white dark:bg-dark-blue" onClick={() => navigate('/')}>Back</button>
+            <button className="text-very-dark-blue-light dark:text-white p-4 rounded-sm text-center mt-4 bg-white dark:bg-dark-blue" onClick={() => navigate('/')}><i class="fa-solid fa-arrow-left"></i> Back</button>
         <div className="flex flex-col md:flex-row justify-between mt-6">
             <div>
-                <img src={countryData[0].flags.svg} alt=""  className="h-[170px]"/>
+                <img src={countryData[0].flags.svg} alt=""  className="h-[170px] pb-6 lg:h-[340px] w-[440px]"/>
             </div>
-            <div>
-                <div className="text-very-dark-blue-light dark:text-white mt-6">
+            <div  className="text-very-dark-blue-light dark:text-white ">
+                <div className="flex flex-col md:flex-row justify-between">
+                    <div>
                     <h2 className=" font-bold text-lg">{countryData[0].name.common}</h2>
-                    <p className="opacity-[0.8] mt-4">Native Name: <span>{countryData[0].name.common}</span></p>
-                    <p className="opacity-[0.8] mt-2">Population: <span>{countryData[0].population.toLocaleString()}</span></p>
-                    <p className="opacity-[0.8] mt-2">Region: <span>{countryData[0].region}</span></p>
-                    <p className="opacity-[0.8] mt-2">Sub Region: <span>{countryData[0].subregion}</span></p>
-                    <p className="opacity-[0.8] mt-2">Capital: <span>{countryData[0].capital}</span></p>
+                    <p className="opacity-[0.8] mt-4">Native Name: <span className="opacity-[0.5]">{countryData[0].name.common}</span></p>
+                    <p className="opacity-[0.8] mt-2">Population: <span className="opacity-[0.5]">{countryData[0].population.toLocaleString()}</span></p>
+                    <p className="opacity-[0.8] mt-2">Region: <span className="opacity-[0.5]">{countryData[0].region}</span></p>
+                    <p className="opacity-[0.8] mt-2">Sub Region: <span className="opacity-[0.5]">{countryData[0].subregion}</span></p>
+                    <p className="opacity-[0.8] mt-2">Capital: <span className="opacity-[0.5]">{countryData[0].capital}</span></p>
+                    </div>
+                    <div>
+                    <p className="opacity-[0.8] mt-4">Top Level Domain:  <span className="opacity-[0.4]">{countryData[0].tld}</span></p>
+                        <p className="opacity-[0.8]">Currencies: <span className="opacity-[0.4]">{Object.keys(countryData[0].currencies)}</span></p>
+                        <p className="opacity-[0.8]">Languages: <span className="opacity-[0.4]">{Object.values(countryData[0].languages).join(',')}</span></p>
+                        </div>
                 </div>
+                {countryData[0].borders && <div className="mt-4 text-very-dark-blue-light dark:text-white">
+                    <h3 className="text-lg">Border Countries</h3>
+                    <div className="flex justify-between mt-4">
+                        {countryData[0].borders.map(border => (
+                            <div key={border} className="bg-white dark:bg-dark-blue rounded-md p-2">
+                                {border}
+                            </div>
+                        ))}
+                    </div>
+                </div>}
+                
             </div>
         </div>
 
